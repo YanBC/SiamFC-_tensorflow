1f8cc65677e497d54f793de00e7eb11636f6cd08
yanbc <imyanbc@gmail.com>
2020-03-11 09:03:08 +0800

    modify dataloader to be compatible with both sequence data and classification data

--- data/dataloader.py
+++ data/dataloader.py
@@ -2,15 +2,11 @@ import numpy as np
 from multiprocessing import Queue, Process, Event, Lock
 
 
-def create_worker(index, sampler, queue, lock, exit_event, neg_ratio=0.1):
+def create_worker(index, sampler, queue, lock, exit_event):
     rng = np.random.RandomState(index)
 
     while not exit_event.is_set():
-        if rng.rand() < neg_ratio:
-            isNegative = True
-        else:
-            isNegative = False
-        data = sampler.sample_one(rng, isNegative)
+        data = sampler.sample_one(rng)
         if lock.acquire(timeout=2):
             try:
                 queue.put(data, timeout=2)
@@ -22,11 +18,11 @@ def create_worker(index, sampler, queue, lock, exit_event, neg_ratio=0.1):
 
 
 class DataLoader:
-    def __init__(self, sampler, num_worker=4, neg_ratio=0.1, buffer_size=6):
+    def __init__(self, sampler, num_worker=4, buffer_size=6):
         self.queue = Queue(buffer_size)
         self.queue_lock = Lock()
         self.exit = Event()
-        self.workers = [Process(target=create_worker, args=(i, sampler, self.queue, self.queue_lock, self.exit, neg_ratio,)) for i in range(num_worker)]
+        self.workers = [Process(target=create_worker, args=(i, sampler, self.queue, self.queue_lock, self.exit,)) for i in range(num_worker)]
 
         for worker in self.workers:
             worker.start()
@@ -45,7 +41,7 @@ if __name__ == '__main__':
         def __init__(self):
             self.numbers = [x for x in range(10)]
 
-        def sample_one(self, rng, isNegative):
+        def sample_one(self, rng):
             return rng.choice(self.numbers)
 
     s = Sample()
