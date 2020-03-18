006bbbc846e0deb557531434af45ff66bae48e50
yanbc <imyanbc@gmail.com>
2020-03-18 15:10:53 +0800

    remove unnecessary codes

--- networks/common.py
+++ networks/common.py
@@ -7,105 +7,6 @@ from tensorflow.compat.v1.nn import depthwise_conv2d
 from tensorflow.compat.v1 import get_variable
 from tensorflow.compat.v1.initializers import he_normal, zeros, ones
 
-from tensorflow.compat.v1.layers import BatchNormalization
-
-
-def conv_bn_relu(input_t,
-                out_channel,
-                name,
-                strides=1,
-                ksize=3,
-                has_bn=True,
-                has_relu=True,
-                has_bias=True):
-    
-    with tf.variable_scope(name, initializer=he_normal(), reuse=tf.AUTO_REUSE):
-        in_channel = input_t.shape[3]
-
-        with tf.variable_scope('weights'):
-            conv_weights = get_variable(
-                                name='weights',
-                                shape=[ksize, ksize, in_channel, out_channel],
-                                trainable=True)
-            if has_bias:
-                conv_bias = get_variable(
-                                name='bias',
-                                shape=[out_channel,],
-                                trainable=True)
-            if has_bn:
-                bn_means = get_variable(
-                                name='means',
-                                shape=[out_channel,],
-                                trainable=True)
-                bn_variances = get_variable(
-                                name='variances',
-                                shape=[out_channel,],
-                                trainable=True)
-                bn_offset = get_variable(
-                                name='offset',
-                                shape=[out_channel,],
-                                trainable=True)
-                bn_scale = get_variable(
-                                name='scale',
-                                shape=[out_channel,],
-                                trainable=True)
-
-        output_t = conv2d(input_t, 
-                        filter=conv_weights,
-                        strides=strides,
-                        padding='VALID',
-                        name='conv')
-        if has_bias:
-            output_t = bias_add(output_t, conv_bias, name='biasadd')
-        if has_bn:
-            output_t = batch_normalization(output_t,
-                                           mean=bn_means,
-                                           variance=bn_variances,
-                                           offset=bn_offset,
-                                           scale=bn_scale,
-                                           variance_epsilon=1e-05,
-                                           name='bn')
-        if has_relu:
-            output_t = relu(output_t, name='relu')
-
-        return output_t
-
-
-
-def xcorr_depthwise(x, kernel, name):
-    with tf.variable_scope(name):
-        net_z = tf.transpose(kernel, perm=[1,2,0,3])
-        net_x = tf.transpose(x, perm=[1,2,0,3])
-
-        Hz, Wz, B, C = tf.unstack(tf.shape(net_z))
-        Hx, Wx, Bx, Cx = tf.unstack(tf.shape(net_x))
-
-        net_z = tf.reshape(net_z, (Hz, Wz, B*C, 1))
-        net_x = tf.reshape(net_x, (1, Hx, Wx, B*C))
-        net_final = depthwise_conv2d(net_x, net_z, strides=[1,1,1,1], padding='VALID', name='correlation')
-
-        _, H, W, _ = tf.unstack(tf.shape(net_final))
-        net_final = tf.reshape(net_final, (H, W, B, C))
-        net_final = tf.transpose(net_final, perm=[2,0,1,3])
-
-        return net_final
-
-
-
-if __name__ == '__main__':
-    import numpy as np
-    np.random.seed(0)
-    image = np.random.rand(8, 256, 26, 26)
-    template = np.random.rand(8, 256, 4, 4)
-
-    image_p = np.transpose(image, axes=[0,2,3,1])
-    template_p = np.transpose(template, axes=[0,2,3,1])
-    x = tf.constant(image_p)
-    z = tf.constant(template_p)
-
-    out = xcorr_depthwise(x, z, name='xor')
-
-
 
 class Conv_Bn_Relu(tf.Module):
     def __init__(self, in_channel, out_channel, name, strides=1, ksize=3, has_bn=True, has_relu=True, has_bias=True):
@@ -236,3 +137,22 @@ class Max_Pooling(tf.Module):
     def __call__(self, input_t):
         output_t = max_pool2d(input_t, ksize=self.ksize, strides=self.strides, padding=self.padding)
         return output_t
+
+
+
+
+if __name__ == '__main__':
+    import numpy as np
+    np.random.seed(0)
+    image = np.random.rand(8, 256, 26, 26)
+    template = np.random.rand(8, 256, 4, 4)
+
+    image_p = np.transpose(image, axes=[0,2,3,1])
+    template_p = np.transpose(template, axes=[0,2,3,1])
+    x = tf.constant(image_p)
+    z = tf.constant(template_p)
+
+    xcorr_depthwise = Xcorr_Depthwise('test')
+    out = xcorr_depthwise(x, z)
+
+
--- networks/models.py
+++ networks/models.py
@@ -1,58 +1,7 @@
-from .backbones import alexnet
-from .adapters import r_z_k, c_z_k, r_x, c_x
-from .common import xcorr_depthwise
-from .heads import denseboxhead
-from .config import network_config as cfg
-
 import tensorflow.compat.v1 as tf
 from tensorflow.compat.v1 import get_variable, constant
 
-from .backbones import Alexnet_Feature
 from .common import Conv_Bn_Relu, Max_Pooling, Fully_Connected, Xcorr_Depthwise
-from tensorflow.compat.v1.initializers import he_normal
-
-
-
-def get_siamfcpp(input_x, input_z):
-    with tf.name_scope('Image'):
-        feature_img = alexnet(input_x)
-        cls_img = c_x(feature_img)
-        reg_img = r_x(feature_img)
-
-    with tf.name_scope('Template'):
-        feature_template = alexnet(input_z)
-        cls_template = c_z_k(feature_template)
-        reg_template = r_z_k(feature_template)
-
-    c_out = xcorr_depthwise(cls_img, cls_template, name='Cls_Xcorr')
-    r_out = xcorr_depthwise(reg_img, reg_template, name='Reg_Xcorr')
-
-    cls_score, ctr_score, bbox = denseboxhead(c_out, r_out)
-
-    return cls_score, ctr_score, bbox
-
-def get_alexnet(input_t):
-    from .common import conv_bn_relu
-
-    assert input_t.shape[1:4] == (224, 224, 3)
-    feature_img = alexnet(input_t)
-
-    with tf.variable_scope('Ad-Hoc', initializer=tf.initializers.he_normal()):
-        output_t = tf.nn.max_pool2d(feature_img, ksize=3, strides=2, padding='VALID')
-        output_t = conv_bn_relu(output_t, name='block_1', out_channel=256, strides=1, ksize=3)
-        output_t = conv_bn_relu(output_t, name='block_2', out_channel=128, strides=1, ksize=3)
-        output_t = tf.layers.flatten(output_t)
-
-        fc1_weights = tf.get_variable('fc1_weights', shape=(2048, 2048), trainable=True)
-        fc2_weights = tf.get_variable('fc2_weights', shape=(2048, 1000), trainable=True)
-
-        output_t = tf.matmul(output_t, fc1_weights)
-        output_t = tf.nn.relu(output_t)
-        output_t = tf.matmul(output_t, fc2_weights)
-        output_t = tf.nn.softmax(output_t)
-
-
-    return output_t
 
 
 class ModelBase(tf.Module):
